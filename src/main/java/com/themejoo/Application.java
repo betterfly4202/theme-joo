f854056b6de9368630c107325e6bd806207c2cc8
이충일 <betterfly4202@gmail.com>
2019-03-23 16:32:44 +0900

    no message

---
+++ src/main/java/com/themejoo/Application.java
@@ -0,0 +1,16 @@
+package com.themejoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by betterfly
+ * Date : 2019.03.23
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
