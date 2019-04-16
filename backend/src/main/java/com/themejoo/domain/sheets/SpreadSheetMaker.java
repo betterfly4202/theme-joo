92555da36c95b0373faf5b020e83f205a80c554a
이충일 <betterfly4202@gmail.com>
2019-04-17 02:08:29 +0900

    - mvn build 오류 처리

--- backend/src/main/java/com/themejoo/domain/sheets/SpreadSheetMaker.java
+++ backend/src/main/java/com/themejoo/domain/sheets/SpreadSheetMaker.java
@@ -23,8 +23,8 @@ public class SpreadSheetMaker {
     @Autowired
     private GoogleConnector googleConnector;
 
-    @Value("${google.sheet.id}")
-    private String sheetId;
+//    @Value("${google.sheet.id}")
+//    private String sheetId;
 
     public SpreadSheetMaker() {
         googleConnector = new GoogleConnector();
@@ -46,6 +46,7 @@ public class SpreadSheetMaker {
 
     public ValueRange readSheets(String range) throws IOException {
         Sheets service = googleConnector.getSheetsService();
+        String sheetId = "1QRZM7G5VN39ftI16dw033zotxNaJwkiQu3aQpsJUK2A";
         ValueRange sheetValues = service.spreadsheets().values().get(sheetId, range).execute();
 
         return sheetValues;
